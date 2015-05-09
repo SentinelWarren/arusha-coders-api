c28284585b85756cf91eb5b5fd3be5f67fdd0851
Reuben Cummings <reubano@gmail.com>
2015-05-09 14:41:26 +0300

    Update readme

--- routes.js
+++ routes.js
@@ -7,33 +7,6 @@ var middleware = require('./middleware')
   , password = require('./controllers/password')
   , config = require('./config')
 
-/* python
-
-// init requirements
-import requests
-
-// create a user
-base = 'http://localhost:3333'
-name, email, password = 'reuben cummings', email', 'password'
-data = {'name': name, 'email': email, 'password': password, 'confirmPassword': password}
-auth = (email, password)
-r = requests.post(base + '/user', data=data)
-r.json()
-
-jwt = r.json()['objects']['jwt']
-headers = {'Authorization': 'Bearer %s' % jwt}
-
-// get user info
-r = requests.get(base + '/user', auth=auth)
-r.json()
-r = requests.get(base + '/user', headers=headers)
-r.json()
-
-// create another token
-r = requests.post(base + '/token', auth=auth)
-r.json()
- */
-
 var exports = module.exports = function(router) {
   router.route('/').get(function(req, res) {
     return res.send("Welcome to the " + config.brand + " API!");
